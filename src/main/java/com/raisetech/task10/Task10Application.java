2acbdcb0daa4e98d2447f4943dd57eb7279830bd
yuika nagata <xxx.yu.74@gmail.com>
2022-11-21 18:05:50 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/raisetech/task10/Task10Application.java
@@ -0,0 +1,13 @@
+package com.raisetech.task10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task10Application.class, args);
+	}
+
+}
